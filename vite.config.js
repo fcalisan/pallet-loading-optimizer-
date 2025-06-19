bbeef2997c00166d24a74e8ba215aa43a41902b8
Edepli Serseri <fatihchalisan@yandex.com>
2025-06-19 07:34:24 +0300

    router

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/demo/palet-yukleme-test/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
